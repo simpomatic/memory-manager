9904f11bd3b5b267d987ae98ec50921d49c06381
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2018-02-24 00:08:59 -0800

    Accounted for free blocks with a exist next block
    Zero progress on splitting memory if there is enough room.

--- v1.1/MemoryManager.c
+++ v1.1/MemoryManager.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
 struct Block
 {
   int block_size;           // # of bytes in the data section
@@ -10,28 +13,31 @@ struct Block *free_head;
 
 void my_initialize_heap(int size)
 {
-  void *buffer = malloc(size);
-  int blockSize = size - (_overheadSize + _pointerSize);
-  (*free_head).block_size = blockSize;
-  (*free_head).next_block = buffer;
-  print((*free_head).block_size);
-  print((*free_head).next_block);
+  free_head = (struct Block*) malloc(size);
+  (*free_head).block_size = size;
+  printf("%s\n", "Initialize Heap:");
+  printf("%p\n", free_head);
+  printf("%d\n", (*free_head).block_size);
 }
 
 void *my_alloc(int size)
 {
-  int allocated = 0;
+  // Flag indicating that we have traversed at least one free block, we need this flag in
+  // the event that memory is allocated in a later block. We will then have the previous block
+  // point to the next available block of memory instead of pointing at memory that is now in use.
+  int counter = 0;
+  int freeSpace = (*free_head).block_size;
   struct Block *currentBlock = free_head;
-  void *allocatedBlock;
-  while (allocated == 0)
+  struct Block *previousBlock;
+  while (!0)
   {
+    // Assuming we can honor this allocation, we are looking at the remaining space minus the
+    // overhead and pointer of a theoretical block to get the ammount of meaningful space left.
+    int remainingSpace = (*currentBlock).block_size - (_overheadSize);
+
     // A pointer that points to nothing will be equal to zero, and !0 is always true.
     if (!(*currentBlock).next_block)
     {
-      // Assuming we can honor this allocation, we are looking at the remaining space minus the
-      // overhead and pointer of a theoretical block to get the ammount of meaningful space left.
-      int remainingSpace = (*currentBlock).block_size - (_overheadSize + _pointerSize);
-
       // Needs to be divisible by four, so increment allocation size until it is.
       while (size % 4 != 0)
       {
@@ -41,6 +47,54 @@ void *my_alloc(int size)
       // Make sure we have enough meaningful space to honor this allocation.
       if (remainingSpace >= size)
       {
+        // Offset free head by size of overhead, pointer size, and the desired amount of memory
+        free_head = currentBlock + (_overheadSize + size);
+        (*free_head).block_size = freeSpace - (_overheadSize + size);
+
+        // Assign values to the allocated block
+        (*currentBlock).block_size = size;
+        (*currentBlock).next_block = free_head;
+        return (void *) (currentBlock + _overheadSize);
+      } else {
+        printf("%s","Not enough memory is available to satisfy this request! Please ensure an acceptable amount is desired.");
+        void *nullPtr;
+        return nullPtr;
+      }
+    } 
+    // This block is free but points to another free block
+    else {
+      // Needs to be divisible by four, so increment allocation size until it is.
+      while (size % 4 != 0)
+      {
+        ++size;
+      }
+
+      // Make sure we have enough meaningful space to honor this allocation.
+      if (remainingSpace >= size)
+      {
+        // The current block is the head of the free list; therefore, simply redirect the free head to
+        // the next block in the chain.
+        if (counter == 0) {
+          free_head = (*currentBlock).next_block;
+          (*free_head).block_size = freeSpace - (_overheadSize + size);
+        } 
+        // The current block is not the first block in the free list; therefore, redirect the previous
+        // block in the change to the next free block in the chain and do not change the free header.
+        else {
+          previousBlock = (*currentBlock).next_block;
+          (*free_head).block_size = freeSpace - (_overheadSize + size);
+        }
+
+        // Assign values to the allocated block
+        (*currentBlock).block_size = size;
+        (*currentBlock).next_block = free_head;
+        return (void *) (currentBlock + _overheadSize);
+      } 
+      // If there is not enough space, check the following block
+      else {
+        previousBlock = currentBlock;
+        currentBlock = (*currentBlock).next_block;
+        ++counter;
       }
     }
   }
@@ -48,5 +102,14 @@ void *my_alloc(int size)
 
 int main()
 {
+  printf("%s\n", "Overhead size:");
+  printf("%d\n", _overheadSize);
+  printf("%s\n", "Pointer size:");
+  printf("%d\n", _pointerSize);
   my_initialize_heap(1000);
+  printf("%s\n", "Allocated Memory location:");
+  printf("%p\n", my_alloc(10));
+  printf("%s\n", "Updated free head location and size:");
+  printf("%p\n", free_head);
+  printf("%d\n", (*free_head).block_size);
 }
